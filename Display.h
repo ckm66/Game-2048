bf74f6e8c3448ecd9094153a774545ff53d432ad
ckm66 <stephen041002@gmail.com>
2022-07-14 11:16:11 +0800

    Update Information Page

--- Display.h
+++ Display.h
@@ -12,11 +12,12 @@ void Information_Page()
 
     box(stdscr, 0, 0);
 
+    int current_x {0};
+    int current_y {0};
+
     attron(A_BOLD);
-    attron(A_REVERSE);
     mvaddstr(1, (x - 20) / 2, "Welcome to Game 2048");
     attroff(A_BOLD);
-    attroff(A_REVERSE);
 
     mvhline(2, 1, '*', x - 2);
 
@@ -25,7 +26,29 @@ void Information_Page()
     mvaddstr(3, 1, "General Description:");
     attroff(A_BOLD);
     mvaddstr(4, 1, "Inspired by One of the most famous game on Earth, 2048, this project aims to bring the old but yet outdated game from Moible Phone to Desktop.");
+    mvaddstr(5, 1, "This Project is original HKU Computer Course Group Project. The Current Vision is revisted and built on my own.");
 
+    mvhline(6, 1, '*', x - 2);
+    
+    attron(A_BOLD);
+    mvaddstr(7, 1, "Achievement:");
+    mvaddstr(8, 1, "-");
+    attroff(A_BOLD);
+
+    
+    mvaddstr(8, 2, " Acheived Project Full Marks");
+
+    mvhline(9, 1, '*', x - 2);
+
+    attron(A_BOLD);
+    mvaddstr(10, 1, "Features: ");
+    mvaddstr(11, 1, "-");
+    attroff(A_BOLD);
+
+    mvaddstr(11, 2, " Written in C++ & NCurses");
+
+
+    
 
     attron(A_BOLD);
     attron(A_REVERSE);
